1a273f72589d43be1b533659110b29f727eff5b5
yannis <yannisdewolf@gmail.com>
2024-04-06 14:31:14 +0200

    init databases

---
+++ docker/america/init/002_insert_data.sql
@@ -0,0 +1,3 @@
+insert into public.city(id, name, population) values (1, 'Austin', 974000 );
+insert into public.city(id, name, population) values (2, 'New York', 8260000 );
+insert into public.city(id, name, population) values (3, 'Washington', 671000 );
